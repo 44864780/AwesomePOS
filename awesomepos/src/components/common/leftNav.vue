9f2e2be0b6373a13dc39d589922f6bad43241984
Ren Wenkun <448267450@qq.com>
2018-03-02 21:33:29 +0800

    first commit

---
+++ awesomepos/src/components/common/leftNav.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="left-Nav">
+      <ul>
+          <li>
+              <i class="icon iconfont icon-goumai"></i>
+              <div>收银</div>
+          </li>
+          <li>
+              <i class="icon iconfont icon-dianpu"></i>
+              <div>店铺</div>
+          </li>
+          <li>
+              <i class="icon iconfont icon-hanbao"></i>
+              <div>商品</div>
+        </li>
+        <li>
+              <i class="icon iconfont icon-huiyuanqia"></i>
+              <div>会员</div>
+        </li>
+        <li>
+              <i class="icon iconfont icon-zuanshi"></i>
+              <div>统计</div>
+        </li>
+        <li>
+              <i class="icon iconfont icon-gongnengjianyi"></i>
+              <div>设置</div>
+        </li>
+      </ul>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style>
+  .left-Nav{
+      color: #fff;
+      font-size: 10px;
+      height: 100%;
+      
+      background-color: #1D8CE0;
+      width: 5%;
+      float: left;
+  }
+.icon iconfont{
+      font-size: 24ox;
+  }
+  .left-Nav ul{
+      padding: 0px;
+      margin: 0px;
+  }
+  .left-Nav li{
+      list-style: none;
+      text-align: center;
+      border-bottom: 1px solid #20a0ff;
+      padding: 10px;
+  }
+</style>
+
+
